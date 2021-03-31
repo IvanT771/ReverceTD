8d7b0de62784617f12fdfe0602129ed31b532f8a
IvanT771 <cnyazev.ivan@yandex.ru>
2021-03-31 19:39:59 +0300

    Add pause and move camera

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster instatiate; //Синголтон
+
+    [Header("Связующие объекты")]
+    [SerializeField] private GameObject panelPause = null;
+
+    private void Awake()
+    {
+        if(instatiate != null) { Debug.LogError("singleton is build!");}
+
+        instatiate = this;
+    }
+
+    public void SetPause(bool isPause)
+    {
+        if(panelPause == null) { return;}
+
+        if (isPause) 
+        { 
+            panelPause.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            panelPause.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
